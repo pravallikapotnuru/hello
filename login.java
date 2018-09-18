aa1a1be2a350fb8d3ed547ce54d72a1d75c8316e
pravallikapotnuru <pravallikapotnuru@gmail.com>
2018-09-18 18:43:13 +0530

    gg

--- login.java
+++ login.java
@@ -2,5 +2,8 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
+      i=10;
+i=20;	      
    }
+
 }
